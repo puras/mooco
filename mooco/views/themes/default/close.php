95b4b7e37816a707d45b98aa575cd0732cec8a0f
puras <puras.he@gmail.com>
2014-12-08 23:49:25 +0800

    default主题的关闭提示页提示信息加padding-top

--- mooco/views/themes/default/close.php
+++ mooco/views/themes/default/close.php
@@ -10,6 +10,7 @@
         background: url("<?php theme_asset_url('img/always_grey.png'); ?>") repeat scroll 0 0 rgba(68, 68, 68, 0.9);
     }
     body.error .logo h1 {
+        padding-top: 40px;
         color: #ffffff;
         font-size: 100px;
         text-shadow: 0 1px 0 #ccc, 0 2px 0 #c9c9c9, 0 3px 0 #bbb, 0 4px 0 #b9b9b9, 0 5px 0 #aaa, 0 6px 1px rgba(0, 0, 0, 0.1), 0 0 5px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.3), 0 3px 5px rgba(0, 0, 0, 0.2), 0 5px 10px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.2), 0 20px 20px rgba(0, 0, 0, 0.15);
