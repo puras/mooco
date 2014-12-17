88dcb2c5eb728b02c61cc8d9bcfb54fee9a7033c
puras <puras@puras-mac.local>
2014-12-17 23:15:51 +0800

    增加Theme

---
+++ mooco/views/themes/lidetech/layout.php
@@ -0,0 +1,229 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">  
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <title><?php echo $site_info->name; ?> -MooCO</title>
+    <meta name="keywords" content="<?php echo $site_seo->key_words; ?>" />
+    <meta name="description" content="<?php echo $site_seo->description; ?>" />
+    <meta name="copyright" content="<?php echo $site_info->copyright; ?>" />
+    <meta name="author" content="cms.mooko.net"/>
+
+    <link rel="stylesheet" type="text/css" href="<?php theme_asset_url('css/lidetech.css'); ?>">
+
+    <script type="text/javascript" src="<?php theme_asset_url('js/jquery.js'); ?>"></script>
+    <script type="text/javascript" src="<?php theme_asset_url('js/lidetech.js'); ?>"></script>
+</head>
+<body id="index">
+    <!-- 头部导航开始 -->  
+    <div id="box_header">
+        <div id="box_header_sub1">
+            <div id="box_header_sub1_sub1">
+                <img src="" width="1141" height="96">
+            </div>
+            <div id="box_header_sub1_sub2">
+                <div class="columnSpace">
+                    <div class="frontColumns_navigation">
+                        <div class="mattblackmenu">
+                            <ul class="nav-first">
+                                <li class="first"><a href="#">网站首页</a></li>
+                                <li><a href="#">企业概况</a></li>
+                                <li><a href="#">企业概况</a></li>
+                                <li><a href="#">企业概况</a></li>
+                                <li><a href="#">企业概况</a></li>
+                                <li><a href="#">企业概况</a></li>
+                                <li><a href="#">企业概况</a></li>
+                                <li class="last"></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="box_header_sub1_sub3">
+                <div id="box_header_sub1_sub3_sub1">
+                    <div class="columnSpace">
+                        <div class="frontPublic_slideShow">
+                            
+                            <div class="slide-contente">
+                                <div class="slide-pic">
+                                    <ul></ul>
+                                </div>
+                                <div class="slide-info-mark"></div>
+                                <div class="slide-info">
+                                    <h3></h3>
+                                    <div class="slide-text">
+                                    </div>
+                                    <div class="slide-tag-text">
+                                        <ul></ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="slide-tag-thumbnail">
+                                <div class="arrowhead-left arrowhead"></div>
+                                <div class="thumbnail-list">
+                                    <ul></ul>
+                                </div>
+                                <div class="arrowhead-right arrowhead"></div>
+                                <div class="clearBoth"></div>
+                            </div>
+
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- 头部导航结束 --> 
+    <div id="box_root" class="pageWidth">
+        <div id="box_main">
+            <div id="box_left">
+                <div id="box_left_sub1">
+                    <div id="box_left_sub1_sub1"></div>
+                    <div id="box_left_sub1_sub2"></div>
+                    <div id="box_left_sub1_sub3"></div>
+                    <div class="clearBoth"></div>
+                </div>
+                <div id="box_left_sub2">
+                    <div id="box_left_sub2_sub1"></div>
+                    <div id="box_left_sub2_sub2"></div>
+                </div>
+                <div id="box_left_sub3">
+                    <div id="box_left_sub3_sub1"></div>
+                    <div id="box_left_sub3_sub2"></div>
+                    <div class="clearBoth"></div>
+                </div>
+            </div>
+            <div class="clearBoth"></div>
+        </div>
+    </div>
+    <div id="box_footer">
+        <div id="box_footer_sub1">
+            <div id="box_footer_sub1_sub1">
+                <div class="columnSpace">
+                    <div class="frontSpecifies_show">
+                        <div class="describe htmledit">
+                            <table cellspacing="0" cellpadding="0" border="0" style="width: 100%">
+                                <tbody>
+                                    <tr>
+                                        <td style="text-align: left;">
+                                            <span style="color: #00507c;">
+                                                <span style="font-size: 14px;">
+                                                    <span style="font-family: 微软雅黑, tahoma, arial, sans-serif;">
+                                                        <a href="">
+                                                            <span style="color: #00507c">
+                                                                <strong>
+                                                                    <font face="">客户留言</font>
+                                                                </strong>
+                                                            </span>
+                                                        </a>
+                                                        <strong>
+                                                            <font face="">&nbsp;&nbsp;|&nbsp;&nbsp;</font>
+                                                        </strong>
+                                                        <a href="">
+                                                            <span style="color: #00507c">
+                                                                <strong>
+                                                                    <font face="">人才招聘</font>
+                                                                </strong>
+                                                            </span>
+                                                        </a>
+                                                        <strong>
+                                                            <font face="">&nbsp;&nbsp;|&nbsp;&nbsp;</font>
+                                                        </strong>
+                                                        <a href="">
+                                                            <span style="color: #00507c">
+                                                                <strong>
+                                                                    <font face="">联系我们</font>
+                                                                </strong>
+                                                            </span>
+                                                        </a>
+                                                    </span>
+                                                </span>
+                                            </span>
+                                        </td>
+                                        <td style="text-align: center;">
+                                            <font face="">
+                                                <span style="color: #707070;">
+                                                    <span style="font-size: 12px;">
+                                                        <span style="font-family: 微软雅黑, tahoma, arial, sans-serif;">
+                                                            联系热线：029-87999234&nbsp;&nbsp;&nbsp;&nbsp;电子邮箱：
+                                                        </span>
+                                                    </span>
+                                                </span>
+                                                <span style="font-size: 12px;">
+                                                    <span style="font-family: 微软雅黑, tahoma, arial, sans-serif;">
+                                                        <a href="">
+                                                            <span style="color: #707070;">he@puras.me</span>
+                                                        </a>
+                                                    </span>
+                                                </span>
+                                                <span style="color: #707070;">
+                                                    <span style="font-size: 12px;">
+                                                        <span style="font-family: 微软雅黑, tahoma, arial, sans-serif;">
+                                                            &nbsp;&nbsp;&nbsp;&nbsp;地址：西安市高新区科技路中段金桥国际广场C座15层
+                                                        </span>
+                                                    </span>
+                                                </span>
+                                            </font>
+                                        </td>
+                                        <td style="text-align: right;">
+                                            <span style="color: #000000;">
+                                                <span style="font-size: 12px;">
+                                                    <span style="font-family: 微软雅黑, tahoma, arial, sans-serif;">
+                                                        <font face="">企业OA | 企业邮箱 | ERP-NC系统</font>
+                                                    </span>
+                                                </span>
+                                            </span>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="box_footer_sub1_sub2">
+                <div id="box_footer_sub1_sub2_sub1">
+                    <div class="columnSpace">
+                        <div class="frontSpecifies_show">
+                            <div class="describe htmledit">
+                                <table border="0" cellpadding="0" cellspacing="0" style="width: 100%">
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <p style="text-align: left;">
+                                                    <a href="www.cmscloud.cc">www.cmscloud.cc</a> . All Right Reserved. <a href="http://www.miibeian.gov.cn/" target="_blank"><?php echo $site_info->icp; ?></a><br />
+                                                    陕西美邦农药有限公司&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://mooko.net" target="_blank">木禾软件提供和技术支持</a>　　<?php link_to('/manage', '后台管理', array('target' => '_black')); ?>
+                                                </p>
+                                            </td>
+                                            <td style="text-align: right;">
+                                                <a href=""><img src="<?php theme_asset_url('img/xxx.jpg'); ?>"></a>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="box_footer_sub1_sub2_sub2">
+                    <div class="columnSpace">
+                        <div class="frontSpecifies_show">
+                            <div class="describe htmledit">
+                                <p style="text-align: right;">
+                                    请让更多的人知道我们!!!<br>
+                                    Please let more people know us!!!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="clearBoth"></div>
+            </div>
+        </div>
+    </div>
+    <?php echo $yield; ?>
+</body>
+</html>
---
+++ mooco/views/themes/lidetech/template_info.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$template_info = (object) array(
+  'show_name'  => 'Lidetech',
+  'author'     => 'Puras.He',
+  'profile'    =>  '默认',
+  'created_at' => '2014-12-16'
+);
