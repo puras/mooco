35304fb3fc02c2bd78a0c474a1b5f6b84668908c
puras <puras.he@gmail.com>
2014-12-25 22:27:47 +0800

    按分类查询时，将其子类的数据一起查询出来

--- mooco/models/article_model.php
+++ mooco/models/article_model.php
@@ -6,18 +6,42 @@ class Article_model extends MK_Model {
         $this->load->model('article_category_model');
     }
 
+    public function find_all_with_child_by_category($category_id) {
+        $ac_ids = array();
+        $ac_ids[] = $category_id;
+
+        $acs = $this->article_category_model->find_all_by(array('parent_id' => $category_id));
+        foreach ($acs as $ac) {
+            $ac_ids[] = $ac->id;
+        }
+
+        $this->db->order_by('publish_time desc');
+        $this->db->where_in('category_id', $ac_ids);
+        return $this->converts($this->db->get($this->get_table_name())->result());
+    }
+
     public function find_all_by_alias($params = array()) {
         $class_alias = array_remove($params, 'category.alias');
         $class = null;
         if ($class_alias != null) {
             $class = $this->article_category_model->find_by(array('alias' => $class_alias));
         }
+        $articles = array();
+
         if ($class != null && isset($class->id)) {
-            $params['category_id'] = $class->id;
-            return parent::find_all_by($params, 'publish_time desc');
+            $articles = $this->find_all_with_child_by_category($class->id);
         }
-        return array();
+
+        return $articles;
+
+        // if ($class != null && isset($class->id)) {
+        //     $params['category_id'] = $class->id;
+        //     return parent::find_all_by($params, 'publish_time desc');
+        // }
+        // return array();
     }
+
+    // private function get
 }
 
 class Article_obj extends MK_Object {
